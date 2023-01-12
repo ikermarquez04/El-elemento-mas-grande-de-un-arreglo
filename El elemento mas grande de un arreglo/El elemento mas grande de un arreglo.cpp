d759682c3e486130ddeb805086bcfa40fbc52680
ikerm <ikerm@LAPTOP-155AQCB8>
2023-01-12 04:03:22 -0600

    Agregar archivos de proyecto.

---
+++ El elemento mas grande de un arreglo/El elemento mas grande de un arreglo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+	int numeros[100], n, mayor = 0;
+
+	cout << "dame los elementos de tu arreglo: ";
+	cin >> n;
+
+	for (int i = 0;i < n < n;i++) 
+	{
+		cout << i + 1 << ". dame un numero: ";
+		cin >> numeros[i];
+	}
+	if (numeros[i] > mayor) {
+		mayor = numeros[i];
+	}
+}
